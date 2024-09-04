9799b74a707d191d31618cb09dc85d7df26dfe42
JuDaCa-UAO <juan_david.carvajal@uao.edu.co>
2024-09-03 19:37:40 -0500

    js

--- basics.js
+++ basics.js
@@ -1,1 +1,29 @@
 console.log("cualquier cosa")
+// que sea let, significa que puede variar a lo largo del programa
+let nombreVariable = "un string"
+// para que sea constante, se pone const
+const variableSinCambios = "algo."
+
+//tipos de datos
+
+let variable = "String";
+console.log(variable);
+let numeroEdad = 25;
+console.log(numeroEdad);
+let variableNula = null;
+let variableNoDefinida = undefined;
+let variableSimbolo = Symbol('unico');
+
+//Estructuras de datos
+//arrays 
+let marcas = ['Mazda', 'Chevrolet', 'Pegeout', 'Ferrari', 'Alfa Romero', 'Subaru', 'Lamborguini', 'Renault', "Toyota", "Ssangyong"];
+console.log (marcas);
+console.log(marcas.length);
+//objetos
+
+let carro ={
+    marca: 'Porche',
+    modelo: '2009',
+    valor: 123456
+}
+console.log(carro);
---
+++ strings.js
@@ -0,0 +1,7 @@
+let string = 'hola';
+let string1 = "mundo";
+let string2 = 'WAPOOOOOOOO';
+
+let string3 = `${string} ${string1} ${string2}`
+
+console.log(string3)
